1604ec46ae050eabb3b828e10ff440bc8393125d
ESSOUCK IRIS <irismarketing427@gmail.com>
2026-06-13 22:15:40 +0100

    Revert to TF-IDF for deployment

--- backend/routes/recommendation_routes.py
+++ backend/routes/recommendation_routes.py
@@ -1,15 +1,19 @@
 from flask import Blueprint, jsonify, request
-from Model.recommend_gcn import recommend_by_user, recommend_by_title
+from Model.recommend import recommend, recommend_by_name
+
+
+# creation du Blueprint
 
 recommendation_bp = Blueprint("recommendation", __name__)
-
-@recommendation_bp.route("/recommend/<int:user_index>", methods=["GET"])
-def get_recommendations(user_index):
-    recommendations = recommend_by_user(user_index)
-    return jsonify({"recommended_movies": recommendations})
-
 @recommendation_bp.route("/recommend_by_name", methods=["GET"])
+
 def search_by_name():
     title = request.args.get("title", "")
-    results = recommend_by_title(title)
+    results = recommend_by_name(title)
     return jsonify({"recommended_movies": results})
+#route recommendations
+@recommendation_bp.route("/recommend/<int:movie_index>", methods=["GET"])
+
+def get_recommendations(movie_index):
+    recommendations= recommend(movie_index)
+    return jsonify({"recommended_movies": recommendations})
